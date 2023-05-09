1506375171a5e69082eb1fc402892c924d5b598e
A1CodeBreaker <129992490+A1CodeBreaker@users.noreply.github.com>
2023-05-09 15:55:05 -0400

    Vehicle search

---
+++ ArrayScripts/my_family.js
@@ -0,0 +1,13 @@
+const myFamily = ['Homer','Marge','Bart','Lisa','Maggie'];
+
+for(let i = 0; i < myFamily.length; i++){
+    console.log(myFamily[i]);
+}
+
+ console.log('done');
+ 
+// myFamily[0] // Homer
+// myFamily[1] // Marge
+// myFamily[2] // Bart
+// myFamily[3] // Lisa
+// myFamily[4] // Maggie
